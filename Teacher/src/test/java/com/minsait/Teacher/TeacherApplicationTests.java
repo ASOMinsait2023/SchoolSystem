1be20be15e199a88b14ad7ac3f6579edd204b87f
maamaror <maamaror@minsait.com>
2023-11-20 14:34:31 -0600

    Create teacher service

---
+++ Teacher/src/test/java/com/minsait/Teacher/TeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsait.Teacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
