e10860f6c95bd34fc22edbb8d3edf6f280172933
Your Name <you@example.com>
2023-11-20 21:55:44 -0600

    First configuration finished

---
+++ Students/src/main/java/com/minsait/Students/models/entities/Career.java
@@ -0,0 +1,24 @@
+package com.minsait.Students.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "careers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Career {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(name = "total_credits")
+    private int totalCredits;
+
+}
---
+++ Students/src/main/java/com/minsait/Students/models/entities/Student.java
@@ -0,0 +1,26 @@
+package com.minsait.Students.models.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "students")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private int age;
+    private String email;
+    @ManyToOne
+    @JoinColumn(name = "career_id", nullable = false)
+    private Career career;
+
+}
