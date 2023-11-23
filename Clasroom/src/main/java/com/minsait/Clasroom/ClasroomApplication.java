8cbec171deb8adb4b68c0b81de85e5184fa26cb0
Your Name <you@example.com>
2023-11-23 11:05:38 -0600

    initial commit

---
+++ Clasroom/src/main/java/com/minsait/Clasroom/ClasroomApplication.java
@@ -0,0 +1,13 @@
+package com.minsait.Clasroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasroomApplication.class, args);
+	}
+
+}
---
+++ Clasroom/src/test/java/com/minsait/Clasroom/ClasroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsait.Clasroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
